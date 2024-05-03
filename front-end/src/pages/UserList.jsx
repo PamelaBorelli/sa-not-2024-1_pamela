e87f9e4a4c640318d9bb9b0255ea69c03e41a986
PamelaBorelli <102415224+PamelaBorelli@users.noreply.github.com>
2024-05-03 18:21:07 -0300

    (27/04) Correção de autorização quebrada de onjeto no backend

--- front-end/src/pages/UserList.jsx
+++ front-end/src/pages/UserList.jsx
@@ -1,44 +1,50 @@
-import React from  'react'
+import React from 'react'
 import myfetch from '../lib/myfetch'
+import './UserList.css'
+import { Link } from 'react-router-dom'
 
-export default function UserList(){
-    const [users, setUsers] = React.useState([])
-    React.useEffect(() => {
-        fetchData()
-    }, [])
+export default function UserList() {
+  const [users, setUsers] = React.useState([])
 
-    async function fetchData(){
-        try{
-            const result = await myfetch.get('/users')
-            setUsers(result)
-        }
-        catch(error){
-            console.log(error)
-            alert('ERRO: ' + error.message)
-        }
+  React.useEffect(() => {
+    fetchData()
+  }, [])
+
+  async function fetchData() {
+    try {
+      const result = await myfetch.get('/users')
+      setUsers(result)
     }
+    catch(error) {
+      console.log(error)
+      alert(error.message)
+    }
+  }
 
-    return(
-        <>
-        <h1>Listagem de usuários</h1>
+  return (
+    <>
+      <h1>Listagem de usuários</h1>
 
-        <table style={{ border: '1px solid black', borderCollapse: 'collapse'}}>
+      <table>
+        <tr>
+          <th>Cód.</th>
+          <th>Nome completo</th>
+          <th>Nome de usuário</th>
+          <th>É admin?</th>
+          <th>Editar</th>
+        </tr>
+        {
+          users.map(u => (
             <tr>
-                <th>Cod.</th>
-                <th>Nome Completo</th>
-                <th>É admin?</th>
+              <td>{u.id}</td>
+              <td>{u.fullname}</td>
+              <td>{u.username}</td>
+              <td>{u.is_admin ? 'Sim' : ''}</td>
+              <td><Link to={`./${u.id}`}>[Editar]</Link></td>
             </tr>
-            {
-                users.map(u =>(
-                    <tr>
-                        <td>{u.id}</td>
-                        <td>{u.fullname}</td>
-                        <td>{u.username}</td>
-                        <td>{u.is_admin ? 'Sim':''}</td>
-                    </tr>
-                ))
-            }
-        </table>
-        </>
-    )
+          ))
+        }
+      </table>
+    </>
+  )
 }
